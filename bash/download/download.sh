09acee202c9f37e5817083e38a8fe3e5591b369d
Kizzyan <yan.leitao26@gmail.com>
2026-01-25 15:20:59 -0300

    feat: Added support for downloading full discographies

--- bash/download/download.sh
+++ bash/download/download.sh
@@ -1,31 +1,58 @@
 #! /bin/sh
 
-ytb="yt-dlp -f bestaudio -x --audio-format mp3 --audio-quality 320k --parse-metadata 'playlist_index:%(track_number)s' --add-metadata --postprocessor-args '-id3v2_version 3'"
-path="$ALX_PATH/.crawler/bash/download/"
-download_path="/media/extra/Music"
+export PATH=/usr/local/bin:/usr/bin:/bin
+export DISPLAY=:0
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+path="$ALX_PATH/.scrafi/bash/download/"
 
-url=$(rofi -dmenu -i -p "󰀥 " -theme-str '@import "'$path'/search.rasi"')
+selected=$(printf "󰇚\n󰧩" | rofi -dmenu -i -theme-str '@import "'$path'/download_1.rasi"')
+case "$selected" in
+  "󰇚")
+    batch=false
+    ;;
+  "󰧩")
+    batch=true
+    ;;
+  *)
+    exit 1
+    ;;
+esac
+
+url=$(rofi -dmenu -i -p "󰀥 " -theme-str '@import "'$path'/download_2.rasi"')
 if [ -n "$url" ]; then
-  json=$(node $ALX_PATH/.crawler/typescript/dist/download.js "$url")
-  artist=$(echo "$json" | jq -r '.artistName')
-  album=$(echo "$json" | jq -r '.albumName')
+  json=$(node "$ALX_PATH"/.scrafi/typescript/scripts/dist/download.js "$url" $batch)
+  if $batch; then
+    artist=$(echo "$json" | jq -r '.artistName')
+    albums=$(echo "$json" | jq -r '.albums')
+  else
+    artist=$(echo "$json" | jq -r '.artistName')
+    album=$(echo "$json" | jq -r '.albumName')
+    year=$(echo "$json" | jq -r '.albumYear')
+  fi
 fi
 
-echo $artist
-echo $album
+download () {
+  if [ ! -d "$DOWNLOAD_PATH/$1" ]; then
+    cd "$DOWNLOAD_PATH" || return
+    mkdir "$1"
+    cd "$1" || return
+    mkdir "$2"
+    cd "$2" || return
+  else
+    cd "$DOWNLOAD_PATH/$1" || return
+    mkdir "$2"
+    cd "$2" || return
+  fi
+  yt-dlp -f bestaudio -x --audio-format mp3 --audio-quality 320k --parse-metadata "playlist_index:%(track_number)s" --add-metadata --postprocessor-args "ffmpeg:-metadata year=$3 -metadata date=$3 -metadata TYER=$3 -id3v2_version 3" "$4"
+}
 
 if [ -n "$url" ]; then
-  if [ ! -d "$download_path/$artist" ]; then
-    cd $download_path
-    mkdir "$artist"
-    cd "$artist"
-    mkdir "$album"
-    cd "$album"
-    yt-dlp -f bestaudio -x --audio-format mp3 --audio-quality 320k --parse-metadata 'playlist_index:%(track_number)s' --add-metadata --postprocessor-args '-id3v2_version 3' $url
-  else
-    cd $download_path/$artist
-    mkdir "$album"
-    cd "$album"
-    yt-dlp -f bestaudio -x --audio-format mp3 --audio-quality 320k --parse-metadata 'playlist_index:%(track_number)s' --add-metadata --postprocessor-args '-id3v2_version 3' $url
+  if $batch; then
+    echo "$albums" | jq -r '.[] | [.name, .url, .year] | @tsv' | while IFS=$'\t' read -r albumName albumUrl albumYear; do
+    download "$artist" "$albumName" "$albumYear" "https://music.youtube.com/$albumUrl"
+  done
+else
+  download "$artist" "$album" "$year" "$url"
   fi
 fi
--- typescript/scripts/download.ts
+++ typescript/scripts/download.ts
@@ -1,11 +1,21 @@
 import puppeteer from "puppeteer";
-import { Util } from "../utils/util";
+import { Util } from "./utils/util";
 
 const url = process.argv[2];
+const isBatch = process.argv[3] == 'true';
 const util = new Util();
+
+interface Album {
+  name: string,
+  url: string,
+  year: string,
+  type: string,
+}
 
 let artistName: string;
 let albumName: string;
+let albumYear: string;
+let albums: Album[];
 
 const getInfo = async () => {
   const browser = await puppeteer.launch({
@@ -22,10 +32,33 @@ const getInfo = async () => {
       waitUntil: "domcontentloaded",
     });
 
-    await page.waitForSelector("ytmusic-thumbnail-renderer.thumbnail:nth-child(3) > yt-img-shadow:nth-child(1) > img:nth-child(1)", { visible: true });
-    artistName = await page.evaluate(() => document.querySelector(".strapline-text > a:nth-child(1)")?.textContent) || "";
-    albumName = await page.evaluate(() => document.querySelector("h1.style-scope:nth-child(6) > yt-formatted-string:nth-child(1)")?.textContent) || "";
-    info = { artistName, albumName };
+    if (isBatch) {
+      await page.waitForSelector("#items > ytmusic-two-row-item-renderer:nth-child(1)", { visible: true, timeout: 10000 });
+      artistName = await page.evaluate(() => document.querySelector("yt-formatted-string.ytmusic-header-renderer")?.textContent) || "";
+      albums = await page.$$eval("#items ytmusic-two-row-item-renderer", (elements) => {
+        return elements.map((element) => {
+          const titleElement = element.querySelector("yt-formatted-string.title");
+          const linkElement = element.querySelector("a");
+          const yearElement = element.querySelector("div:nth-child(4) > span:nth-child(2) > yt-formatted-string:nth-child(2) > span:nth-child(3)");
+          const typeElement = element.querySelector("div:nth-child(4) > span:nth-child(2) > yt-formatted-string:nth-child(2) > span:nth-child(1)");
+
+          return {
+            name: titleElement?.textContent?.trim() || "",
+            url: linkElement?.getAttribute("href") || "",
+            year: yearElement?.textContent?.trim() || "",
+            type: typeElement?.textContent?.trim() || ""
+          };
+        }).filter(album => album.name && album.url && album.year && album.type != "Single");
+      });
+
+      info = { artistName, albums };
+    } else {
+      await page.waitForSelector("ytmusic-thumbnail-renderer.thumbnail:nth-child(3) > yt-img-shadow:nth-child(1) > img:nth-child(1)", { visible: true });
+      artistName = await page.evaluate(() => document.querySelector(".strapline-text > a:nth-child(1)")?.textContent) || "";
+      albumName = await page.evaluate(() => document.querySelector("h1.style-scope:nth-child(6) > yt-formatted-string:nth-child(1)")?.textContent) || "";
+      albumYear = await page.evaluate(() => document.querySelector("yt-formatted-string.ytmusic-responsive-header-renderer:nth-child(2) > span:nth-child(3)")?.textContent) || "";
+      info = { artistName, albumName, albumYear };
+    }
 
     console.log(JSON.stringify(info));
   } catch (error) {
